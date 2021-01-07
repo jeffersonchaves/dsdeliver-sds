246920680f23f0119264aa993475a777ee966849
jefferson.chaves <jeffersong.s@hotmail.com>
2021-01-07 19:45:56 -0300

    project created

---
+++ dsdeliver-backend/src/main/java/ifpr/edu/edu/sds/dsdeliver/DsdeliverApplication.java
@@ -0,0 +1,13 @@
+package ifpr.edu.edu.sds.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliverApplication.class, args);
+	}
+
+}
