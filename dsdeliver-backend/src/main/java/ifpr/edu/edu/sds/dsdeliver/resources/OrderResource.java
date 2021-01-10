3e3ba1c936479a1185a4259fbd4fa6f7f9d213bc
jefferson.chaves <jeffersong.s@hotmail.com>
2021-01-09 21:39:29 -0300

    PUT /order/{id}/delivered

--- dsdeliver-backend/src/main/java/ifpr/edu/edu/sds/dsdeliver/resources/OrderResource.java
+++ dsdeliver-backend/src/main/java/ifpr/edu/edu/sds/dsdeliver/resources/OrderResource.java
@@ -6,13 +6,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import ifpr.edu.edu.sds.dsdeliver.DTO.OrderDTO;
+import ifpr.edu.edu.sds.dsdeliver.entities.Order;
 import ifpr.edu.edu.sds.dsdeliver.services.OrderService;
 
 @RestController
@@ -39,4 +42,9 @@ public class OrderResource {
 		
 	}
 	
+	@PutMapping("/{id}/delivered")
+	public  ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id){
+		OrderDTO dto = service.setDelivered(id);
+		return ResponseEntity.ok().body(dto);
+	}
 }
